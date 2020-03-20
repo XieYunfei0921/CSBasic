b50375e6afdac398f04344101c7bf47ea952d3ff
XieYunfei0921 <XieYunfei0921@126.com>
2020-03-20 19:45:37 +0800

    Create CriticalPath.cpp

---
+++ 数据结构/Code/CriticalPath.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// 关键路径求解
+// 使用AOE网求解
+struct Edge{
+    int st;// 起始节点
+    int ed;// 结束节点
+    int ts;// 活动的时间
+    int degree;// 表示以开始节点st的入度
+};
+// 拓扑排序
+void toplogicOrder(){
+
+}
+
+void getCriticalPath(){
+
+}
+int main(int argc, char const *argv[]){
+
+    return 0;
+}
